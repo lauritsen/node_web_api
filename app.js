5fb9dd0a22db5114b0e59d3124bbbbb73f822f87
Henrik Lauritsen <lauritsen@gmail.com>
2020-11-02 13:19:29 +0100

    added app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my node api!');
+});
+
+app.listen(port, () => {
+    console.log(`Running on node api ${port}`);
+});
